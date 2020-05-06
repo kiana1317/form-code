dc6b88820448c0d367c76d2458f2ac50f7e756f7
kiana <kiana.hobbs@gmail.com>
2020-05-06 13:47:45 -0500

    Updated version with interactivity

--- js/main.js
+++ js/main.js
@@ -4,25 +4,50 @@ let xs = Array(100).fill(innerWidth / 2)
 let ys = Array(100).fill(innerHeight / 2)
 let dxs = []
 let dys = []
+let rec = false
+let trans = 100
+let change = 5
+let pressed = false
 
 // Setup appears at page load
 function setup() {
   createCanvas(innerWidth, innerHeight)
-  background(0,128,128, 100) // Last color is transparency
+  background(0,128,128, trans) // Last color is transparency
   dxs = Array.from({length: 100}, () => random(-2,2));
   dys = Array.from({length: 100}, () => random(-2,2));
 }
 
-function drawOverlaps(x,y){
-  stroke(random(100,255))
-  for(let i = 0; i < total; i++){
-    let distance = dist(x,y, xs[i], ys[i])
-    if (distance > d) line(x,y, xs[i], ys[i])
+function keyPressed() {
+  if (keyCode === 32) {
+    if (pressed) loop() else(noLoop())
+    pressed = !pressed
+  }
+  if (keyCode === UP_ARROW){
+    trans += change
+  }
+  if (keyCode === DOWN_ARROW){
+    trans -= change
   }
 }
 
+function drawOverlaps(x,y){
+  stroke(random(100,255))
+  noFill()
+  for(let i = 0; i < total; i++){
+    let distance = dist(x,y, xs[i], ys[i])
+    if (distance < d) {
+      if (rec) {rect(x,y, xs[i], ys[i])} else ellipse(x,y, xs[i], ys[i])
+    }
+  }
+}
+function mousePressed(){
+  xs = Array(100).fill(mouseX)
+  ys = Array(100).fill(mouseY)
+  rec = !rec
+}
+
 function draw() {
-  background(0,128,128, 10)
+  background(0,128,128, trans)
   for(let i = 0; i < total; i++){
     xs[i] += dxs[i]
     ys[i] += dys[i]
